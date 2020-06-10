408526e5819e2d0add86e777081c8aee3b54aab4
Ghassen <liardusk@gmail.com>
2020-06-10 07:59:41 +0100

    Add Termcolor and fix some bugs

--- mangadown.py
+++ mangadown.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests , shutil, os
-oriDir = r"C:\Users\Ghass\Desktop\mangadown"
+from termcolor import colored, cprint 
+oriDir = os.getcwd()
 def downloader(url):
     r = requests.get(url)
     c = r.content
@@ -14,7 +15,7 @@ def downloader(url):
         del pageurl[-1]
         del pageurl[-1]
         npageurl = "/".join(pageurl)+"/" + str(i)
-        print(npageurl)
+        cprint(npageurl,"magenta")
         imgurl = getImgurl(npageurl)
         pagename = imgurl.split("/")[-1]
         r = requests.get(imgurl, stream = True)
@@ -30,28 +31,36 @@ def getImgurl(url):
     c = r.content
     soup = bs(c,"html.parser")
     return soup.find("div",{"id":"showchaptercontainer"}).find("a").find("img")["src"]
-search = input("Enter a manga to search  ")
+cprint("---------------------------------------------" , "magenta")    
+cprint("----------  Manga Down By Ghassen  ----------","magenta","on_white")
+cprint("---------------------------------------------" , "magenta")
+cprint("Enter a manga name ","white")
+cprint("------------------","magenta")
+search = input()
 sl = "https://manga.ae/manga/search:" + search
 r = requests.get(sl)
 c = r.content
 soup = bs(c,"html.parser")
 mcontainer = soup.find_all("div",{"class":"mangacontainer"})
 if len(mcontainer) < 1:
-    print("No Manga With that name Found !!")
+    cprint("Zannen . No Manga with same name Found !!","red")
 else :
-    print ("Those mangas are found , Select one")
+    
+    cprint("---------------------------------------------" , "magenta")
     for i in mcontainer :
         link = i.find("a")["href"]
         linklen = len(link)
         dash = "-"*int(linklen/2)
-        print(dash + str(mcontainer.index(i)+1) + dash)
-        name = i.find_all("a")[1].text
-        print(name)
-        namear = i.find_all("a")[2].text
-        print(namear)
         
-        print(link)
-    selected = int(input("select a number  "))-1
+        name = i.find_all("a")[1].text
+        cprint(str(mcontainer.index(i)+1)+ " - " +name,"magenta")
+        # namear = i.find_all("a")[2].text
+        
+        cprint("---------------------------------------------" , "magenta")
+        # cprint(link,"white")
+    cprint("Yuupi , Select one Onegaishimassu","green")    
+    cprint("Choose a number ..  ","green")    
+    selected = int(input())-1
     ml = mcontainer[selected].find("a")["href"]
     mn = mcontainer[selected].find_all("a")[1].text.replace(":","")
     nc = requests.get(ml).content
@@ -59,30 +68,49 @@ else :
     chaptersbox = nsoup.find("ul",{"class":"new-manga-chapters"})
     chapters = chaptersbox.find_all("li")
     chapters.reverse()
-    print(str(len(chapters)) + " chapters Found...")
+    cprint(str(len(chapters)) + " Chapters found...","grey")
     os.mkdir(mn)
-    
-    dwchoice = int(input("1 Download All   2 Download by chapter  3 download by range"))
+    cprint("1 Download all   2 Download by chapter  3 Download by range","magenta")
+    dwchoice = int(input())
     if  dwchoice == 1:
+        x=1
         for chapter in chapters :
+            
             os.chdir(oriDir+"/"+mn)
             cl = chapter.find("a")["href"]
-            downloader(cl)
-            
+            try:
+                downloader(cl)
+                cprint("Reminder : To force stop press Ctrl+C twice","red")
+            except:
+                pass
+            cprint(str(x)+" Chapters Downloaded")
+            x+=1
     elif dwchoice == 2 :
         os.chdir(oriDir+"/"+mn)
-        choosenchap = int(input("choose a chapter  "))
+        cprint("Choose one chapter","green" )
+        choosenchap = int(input())
         cl = chapters[choosenchap-1].find("a")["href"]
-        downloader(cl)
-        
+        try :
+            downloader(cl)
+        except:
+            pass    
+        cprint("One Chapter Downloaded")
     elif dwchoice == 3 :
-        Rfrom = int(input("choose a starting chapter  "))
-        Rto = int(input("choose an End chapter  "))
+        cprint("choose first chapter","green")
+        Rfrom = int(input())
+        cprint("choose last chapter","green")
+        Rto = int(input())
+        x=1
         for i in range (Rfrom, Rto+1):
             os.chdir(oriDir+"/"+mn)
             cl = chapters[i-1].find("a")["href"]
-            downloader(cl)
+            try:
+                downloader(cl)
+            except:
+                pass
+            cprint(str(x)+" Chapters Downloaded")    
+            x+=1
             
         
-    
+   
         
